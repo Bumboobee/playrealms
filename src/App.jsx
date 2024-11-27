7a929604d4f70e450852c12ac984d7ddc7876ce9
Jose Aparecido <juniorsilva240803@gmail.com>
2024-11-27 13:52:12 -0300

    feat: about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Hero from "./components/hero";
+import About from "./components/about";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
 
-      <section className="z-0 min-h-screen bg-blue-500"></section>
+      <About />
     </main>
   );
 };
